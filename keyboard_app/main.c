d7d8ee25764a6025186f58eab22816028b9894a2
Carlos German Carreño Romano <charlieromano@gmail.com>
2024-08-04 20:11:05 +0100

    "correcting example4"

--- keyboard_app/main.c
+++ keyboard_app/main.c
@@ -2,12 +2,10 @@
 #include <glib/gstdio.h>
 
 static void
-print_A (GtkWidget *widget, gpointer data){
-	g_print ("A\n");
-}
-static void
-print_B (GtkWidget *widget, gpointer data){
-	g_print ("B\n");
+print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+  g_print ("Hello World\n");
 }
 
 static void
@@ -17,43 +15,45 @@ quit_cb (GtkWindow *window)
 }
 
 static void
-activate (GtkApplication *app, gpointer user_data){
+activate (GtkApplication *app,
+          gpointer        user_data)
+{
+  /* Construct a GtkBuilder instance and load our UI description */
+  GtkBuilder *builder = gtk_builder_new ();
+  gtk_builder_add_from_file (builder, "builder.ui", NULL);
 
-	GtkBuilder *builder = gtk_builder_new();
-	gtk_builder_add_from_file (builder, "builder.ui", NULL);
+  /* Connect signal handlers to the constructed widgets. */
+  GObject *window = gtk_builder_get_object (builder, "window");
+  gtk_window_set_application (GTK_WINDOW (window), app);
 
-	GObject *window = gtk_builder_get_object (builder, "window");
-	gtk_window_set_application (GTK_WINDOW (window), app);
+  GObject *button = gtk_builder_get_object (builder, "button1");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
 
-	GObject *button = gtk_builder_get_object (builder, "buttonA");
-	g_signal_connect (button, "clicked", G_CALLBACK (print_A), NULL);
+  button = gtk_builder_get_object (builder, "button2");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
 
-	button = gtk_builder_get_object (builder, "buttonB");
-	g_signal_connect (button, "clicked", G_CALLBACK (print_B), NULL);
+  button = gtk_builder_get_object (builder, "quit");
+  g_signal_connect_swapped (button, "clicked", G_CALLBACK (quit_cb), window);
 
-	button = gtk_builder_get_object (builder, "quit");
-	g_signal_connect (button, "clicked", G_CALLBACK (quit_cb), window);
+  gtk_widget_set_visible (GTK_WIDGET (window), TRUE);
 
-	gtk_widget_set_visible (GTK_WIDGET (window), TRUE);
-
-	gtk_object_unref (builder);
+  /* We do not need the builder any more */
+  g_object_unref (builder);
 }
 
-int main (int argc, char **argv){
+int
+main (int   argc,
+      char *argv[])
+{
 #ifdef GTK_SRCDIR
-	g_chdir (GTK_SRCDIR);
+  g_chdir (GTK_SRCDIR);
 #endif
 
-	GtkApplication *app;
-	int status;
+  GtkApplication *app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
 
-	app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS );
-	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-	status = g_application_run (G_APPLICATION (app), argc, argv );
-	g_object_unref (app);
+  int status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
 
-	return status;
+  return status;
 }
-
-	
-
