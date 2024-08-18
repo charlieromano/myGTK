d83d07bbae9fcaebfc136e965542e74c668abb3e
charlieromano <charlieromano@gmail.com>
2024-08-18 16:32:54 +0100

    deleting submodule gtk

--- GTK/gtk/compose/gtkcomposedata.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define MAX_SEQ_LEN 5
-#define N_INDEX_SIZE 39
-#define DATA_SIZE 15777
-#define N_CHARS 1321
-
--- GTK/tests/frame-stats.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <gtk/gtk.h>
-
-void frame_stats_add_options (GOptionGroup *group);
-void frame_stats_ensure      (GtkWindow    *window);
