acfe819e64544df59529346b1ef21f351ee4bc1c
charlieromano <charlieromano@gmail.com>
2024-08-18 16:40:39 +0100

    deleting submodule gtk

--- GTK/gsk/gskrendernodeparserprivate.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-
-#include "gskrendernode.h"
-
-GskRenderNode * gsk_render_node_deserialize_from_bytes  (GBytes            *bytes,
-                                                         GskParseErrorFunc  error_func,
-                                                         gpointer           user_data);
--- GTK/tests/dummy-headers/windows.h
+++
@@ -1,5 +0,0 @@
-/* Dummy header for the Win32 backend. */
-
-typedef void *HDC;
-typedef void *HICON;
-typedef void *HWND;
